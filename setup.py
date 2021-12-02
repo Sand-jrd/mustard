fa18270e40572858bfe95455d4941fe311184605
Sand-jrd <s.juillard@hotmail.fr>
2021-12-02 16:01:56 +0100

    forgot something for setup

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from pip._internal.req import parse_requirements
 
 
 reqs = parse_requirements("./requirements.txt", session=False)
+try:
+    reqs = [str(ir.req) for ir in reqs]
+except:
+    reqs = [str(ir.requirement) for ir in reqs]
 
 setup(
     version="1.0",
