2073365c7b1c038efd547783a86524bb829e071d
Sand-jrd <47563242+Sand-jrd@users.noreply.github.com>
2023-05-05 11:15:43 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -13,8 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.append('/Users/sand-jrd/GitProjects/my_projects/mustard/')
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
